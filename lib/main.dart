96374cbba76fbd358befdfe96ac9ea9888e1f9c0
Muhamamd Jawad <m.jawad8991@gmail.com>
2022-01-25 11:27:07 +0500

    Adding splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/widgets/custom_fade_transition.dart';
+import 'package:portfolio/widgets/custom_text.dart';
 import 'package:url_strategy/url_strategy.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import '../views/mobile/mobile_content.dart';
 import '../routes/route_generator.dart';
 import '../routes/routes.dart';
@@ -33,8 +35,65 @@ class MyApp extends StatelessWidget {
           Theme.of(context).textTheme,
         ),
       ),
-      home: const LoadingScreen(),
+      home: const App(),
     );
+  }
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Init.instance.initialize(),
+        builder: (context, AsyncSnapshot snapshot) {
+          // Show splash screen while waiting for app resources to load:
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Splash();
+          } else {
+            // Loading is done, return the app:
+            return const MainScreen();
+          }
+        });
+  }
+}
+
+class Splash extends StatelessWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // bool lightMode = MediaQuery.of(context).platformBrightness == Brightness.light;
+    return Scaffold(
+        backgroundColor: AppColors.greenColor,
+        // lightMode ?  AppColors.greenColor : const Color(0xff042a49),
+        body: CustomFadeTransition(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/icon.png'),
+                CustomText(text: 'Muhammad Jawad',fontSize: 25,)
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class Init {
+  Init._();
+
+  static final instance = Init._();
+
+  Future initialize() async {
+    // This is where you can initialize the resources needed by your app while
+    // the splash screen is displayed.  Remove the following example because
+    // delaying the user experience is a bad design practice!
+    await Future.delayed(const Duration(seconds: 3));
   }
 }
 
